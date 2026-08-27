cacb24567b11f03b457c306e2679622fdfa17cee
Kilian Koeltzsch <me@kilian.io>
2026-08-28 00:13:59 +0200

    chore: format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
-      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
       forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
-    in {
+    in
+    {
       devShells = forAllSystems (pkgs: {
         default = pkgs.mkShell {
           packages = [
